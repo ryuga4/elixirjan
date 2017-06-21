3a87247ac44e2944a34513c2121701ee32489af9
ryuga4 <ryuga4@o2.pl>
2017-06-21 18:21:07 +0200

    add player

--- lib/hello_phoenix/auto_pilot.ex
+++ lib/hello_phoenix/auto_pilot.ex
@@ -6,4 +6,10 @@ defmodule AutoPilot do
     GenServer.start_link(__MODULE__, %{})
     Agent.start_link(fn -> [] end,name: __MODULE__)
   end
+
+  def add_player(%HelloPhoenix.Player{name: name}) do
+    Agent.update(__MODULE__,&([name,&1]))
+  end
+
+
 end
